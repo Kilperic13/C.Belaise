c9aeaa356ed7b84583c985d1a6bcd90944cac1d9
Kilperic <kilpericn@gmail.com>
2020-03-27 15:31:11 -0400

    Modification on the file 'CreaMarkVirt-V3', traduction in english, very bad, and modification to let you the option how do you want to creat your markers : impose torques or angles in your model

--- V3/CreaMarkVirt-V3.py
+++ V3/CreaMarkVirt-V3.py
@@ -5,15 +5,18 @@ from BiorbdViz import BiorbdViz
 import scipy.interpolate
 import scipy.integrate
 import numpy as np
+import conf as conf
 
-T = 3.2
-N = 1000
+T = conf.T
+N = conf.N
+CM = conf.CM
 dN = T/N
 
-model = biorbd.Model("/home/lim/Documents/code/Models/V3/arm26.bioMod")
-#kni
+model = biorbd.Model("/home/lim/Documents/code/Models/V6/arm26.bioMod")
+#coucou
 
-# Creation des fonctions integrals
+# Creation of integrals functions
+
 
 NbQ = model.nbQ()
 NbQd = model.nbQdot()
@@ -31,17 +34,12 @@ def func_ode(t, x, tau1, tau2):
 
 
 def INT(Xk, tau):
-    # V0 = Xk[1]
-    # X0 = Xk[0]
-    # Total = Xk[0] + Xk[1] + tau
-    Total = Xk[0] + Xk[1]
-
     t = np.linspace(0, dN, 100)
 
-    #sol_Xk = scipy.integrate.solve_ivp(func_ode, [0, dN], Total, method='RK45')
     sol_Xk = scipy.integrate.solve_ivp(func_ode, [0, dN], Xk, method='RK45', args=(tau[0], tau[1]))
-    # sol_interp_Xk = scipy.interpolate.interp1d(sol_Xk.t, sol_Xk.y, kind='cubic')
 
+    # not developed yet
+    # sol_interp_Xk = scipy.interpolate.interp1d(sol_Xk.t, sol_Xk.y, kind='cubic')
     # print(sol_interp_V(t))
     # plt.plot(t, sol_interp_V(t).squeeze())
     # plt.show(block=True)
@@ -49,58 +47,70 @@ def INT(Xk, tau):
     return sol_Xk.y
 
 
-# Definition du temps et de la position au articulation
+# Definition of time and angles of articulations
+
 
 tps = np.linspace(0, T, N)
-Q1 = np.cos(tps)
-Q2 = Q1/2.
-Q = np.vstack([Q1, Q2])
-V = np.gradient(Q, dN, edge_order = 1, axis=1)
+
+# Creation base on the angle
+if CM == 0:
+    Q1 = np.cos(tps)
+    # Q1 = conf.fct_CM(tps)
+    Q2 = Q1/2.
+    Q = np.vstack([Q1, Q2])
+    V = np.gradient(Q, dN, edge_order = 1, axis=1)
+
+# Creation base on the Torque
+elif CM == 1:
+    T1 = 10*np.tanh(tps)
+    # T1 = 10 * conf.fct_CM(tps)
+    T2 = 5*np.tanh(tps)
+    # T2 = 5 * conf.fct_CM(tps)
+    V = np.zeros((2, 1)).squeeze()
+    Q = np.array([1, 0.5])
+    Xk = []
+    Nbis = N
+    while Nbis != 0:
+        Xk = INT(np.append(Q[N - Nbis:], V[N - Nbis:]), [T1[N - Nbis], T2[N - Nbis]])
+        Q = np.vstack([Q, Xk[:NbQ, len(Xk[NbQd-1])-1]])
+        V = np.vstack([V, Xk[NbQ:, len(Xk[NbQd-1])-1]])
+        Nbis -= 1
+
+    V_Test = np.gradient(Q, dN, edge_order = 1, axis=0)          # By CURIOSITY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
-# Definition des couples articulaires
+# Definition of markers positions in array type. Argument : Step calculation, Number of Marker, Coordinate xyz
 
-Tart_opt = np.load('Couple_Opt_V3.npy')
-T1 = Tart_opt[4::6]
-T2 = Tart_opt[5::6]
-Vbis = np.zeros((2, 1)).squeeze()
-Qbis = np.array([1, 0.5])
-Xk = []
-Nbis = N
-while Nbis != 0:
-    Xk = INT(np.append(Qbis[N - Nbis:], Vbis[N - Nbis:]), [T1[N - Nbis], T2[N - Nbis]])
-    Qbis = np.vstack([Qbis, Xk[:NbQ, len(Xk[NbQd-1])-1]])
-    Vbis = np.vstack([Vbis, Xk[NbQ:, len(Xk[NbQd-1])-1]])
-    Nbis -= 1
+if CM == 1:
+    markers = [[model.markers(np.array(Q[k]))[j].to_array() for j in range(len(model.markers(np.array(Q[k]))))] for k in range(N)]
+elif CM == 0:
+    markers = [[model.markers(np.array(Q.T[k]))[j].to_array() for j in range(len(model.markers(np.array(Q.T[k]))))] for k in
+               range(len(Q1))]
 
-Vbis_Test = np.gradient(Qbis, dN, edge_order = 1, axis=0)          # Par curiosité
+# if CM == 0 :
+#     np.save('DataMarkeur.npy', markers)
+#     NewMarkers = np.load('DataMarkeur.npy')
+# elif CM == 1 :
+#     np.save('DataMarkeur-Couple.npy', markers)
+#     NewMarkers = np.load('DataMarkeur-Couple.npy')
 
-
-# Definition de la position des markers en array. argument : Pas de calcul, Numero Marker, Coordonne xyz
-
-markers = [[model.markers(np.array(Qbis[k]))[j].to_array() for j in range(len(model.markers(np.array(Qbis[k]))))] for k in range(len(Q1))]
-
-np.save('DataMarkeur-Couple.npy', markers)
-
-NewMarkers = np.load('DataMarkeur-Couple.npy')
-print(NewMarkers)
+# print(NewMarkers)
 # print(Q)
-print(markers)
+# print(markers)
 
-V1 = np.gradient(Q1, tps, edge_order = 1)                      # Approximation plus vrai, edge_order peut être egal a 2, change le premier et dernier terme
-Acc = np.gradient(V1, tps, edge_order = 1)
+# V1 = np.gradient(Q1, tps, edge_order = 1)                      # Approximation plus vrai, edge_order peut être egal a 2, change le premier et dernier terme
+# Acc = np.gradient(V1, tps, edge_order = 1)
 
-# Affichage creation plt
 
-plt.plot(tps, Q.T, label = 'Position')
-plt.plot(tps, V.T, label = 'Vitesse')
-plt.title('Position / Vitesse Initial - Cos')
-plt.legend(loc='best')
-plt.show(block=True)
-plt.figure()
-# plt.title('Position / Vitesse Initial')
-# plt.legend(loc='best')
-# plt.ion()
+# Display : creation of plt
+
+if CM == 0 :
+    plt.plot(tps, Q.T, label = 'Position')
+    plt.plot(tps, V.T, label = 'Vitesse')
+    plt.title('Position / Vitesse Initial')
+    plt.legend(loc='best')
+    plt.show(block=True)
+    plt.figure()
 # plt.plot(tps, Q1, label='Q_1')
 # plt.plot(tps, Q2, label='Q_2')
 # plt.title('Position Initial')
@@ -110,29 +120,35 @@ plt.figure()
 # plt.title('Vitesse Initial')
 # plt.legend(loc='best')
 # plt.show(block=True)
-plt.plot(tps, Qbis[:-1, 0], label = 'Position 0 tan')
-plt.plot(tps, Qbis[:-1, 1], label = 'Position 1 tan')
-plt.plot(tps, Vbis[:-1, 0], label = 'Vitesse 0 tan')
-plt.plot(tps, Vbis[:-1, 1], label = 'Vitesse 1 tan')
-# plt.plot(tps, Vbis_Test[:-1, 0], label = 'Vitesse Test 0 tan')
-# plt.plot(tps, Vbis_Test[:-1, 1], label = 'Vitesse Test 1 tan')
-plt.title('Position Initial')
-plt.legend(loc='best')
-plt.show(block=True)
-plt.figure()
+elif CM == 1 :
+    plt.plot(tps, Q[:-1, 0], label = 'Position 0 tan')
+    plt.plot(tps, Q[:-1, 1], label = 'Position 1 tan')
+    plt.plot(tps, V[:-1, 0], label = 'Vitesse 0 tan')
+    plt.plot(tps, V[:-1, 1], label = 'Vitesse 1 tan')
+    # plt.plot(tps, Vbis_Test[:-1, 0], label = 'Vitesse Test 0 tan')
+    # plt.plot(tps, Vbis_Test[:-1, 1], label = 'Vitesse Test 1 tan')
+    plt.title('Position Initial')
+    plt.legend(loc='best')
+    plt.show(block=True)
+    plt.figure()
+    plt.plot(tps, T1, label = 'Couple 0 tan')
+    plt.plot(tps, T2, label = 'Couple 1 tan')
+    plt.title('Initial Torque')
+    plt.legend(loc='best')
+    plt.show(block=True)
 
 
-# Affichage creation Model
+# Display : creation biorbd-Viz
 
-
-qs = np.array([Qbis[:-1, 0], Qbis[:-1, 1]])                                           # argument = les different angle de ton model par DDL
-np.save("visual", qs.T)                                                               # qs.T pour transposer de qs
-b = BiorbdViz(model_path="/home/lim/Documents/code/Models/V3/arm26.bioMod")           # Va chercher ton modele
-b.load_movement(qs.T)
-b.exec()
-
-# qs = np.array([Q1, Q2])                                                               # argument = les different angle de ton model par DDL
-# np.save("visual", qs.T)                                                               # qs.T pour transposer de qs
-# b = BiorbdViz(model_path="/home/lim/Documents/code/Models/V3/arm26.bioMod")           # Va chercher ton modele
-# b.load_movement(qs.T)
-# b.exec()
+if CM == 1:
+    qs = np.array([Q[:-1, 0], Q[:-1, 1]])                                                   # argument = different angle of you model by DoF
+    np.save("visual", qs.T)                                                                 # qs.T is the transposed matrix to the matrix qs
+    b = BiorbdViz(model_path="/home/lim/Documents/code/Models/V6/arm26.bioMod")             # find your model
+    b.load_movement(qs.T)
+    b.exec()
+elif CM == 0:
+    qs = np.array([Q1, Q2])                                                               # argument = different angle of you model by DoF
+    np.save("visual", qs.T)                                                               # qs.T is the transposed matrix to the matrix qs
+    b = BiorbdViz(model_path="/home/lim/Documents/code/Models/V6/arm26.bioMod")           # find your model
+    b.load_movement(qs.T)
+    b.exec()
